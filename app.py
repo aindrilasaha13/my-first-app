807fca97ca397bd083845de89964e2d969d3e339
Aindrila <aindrila.saha.kgec@gmail.com>
2022-04-01 11:56:06 +0530

    tetsing images

--- app.py
+++ app.py
@@ -10,24 +10,25 @@ app = Flask(__name__)
 def add():
     return render_template("home.html")
 
-# @app.post("/name")
-# def name():
-#     text_response = requests.get("https://testing-first-flask-app.herokuapp.com//hello")
-#     name = request.form.get("name")
-#     display_string = text_response.text + " "+name
+@app.post("/name")
+def name():
+    text_response = requests.get("https://testing-first-flask-app.herokuapp.com//hello")
+    name = request.form.get("name")
+    display_string = text_response.text + " "+name
     
-#     img_response = requests.get("https://testing-first-flask-app.herokuapp.com//image")
-#     file = open("static\\downloaded.jpg", "wb")
-#     file.write(img_response.content)
-#     file.close()
-#     img = "/static/downloaded.jpg"
-#     return render_template("new_page.html",img=img)
+    # img_response = requests.get("https://testing-first-flask-app.herokuapp.com//image")
+    # file = open("static\\downloaded.jpg", "wb")
+    # file.write(img_response.content)
+    # file.close()
+    # img = "/static/downloaded.jpg"
+    img = requests.get("https://testing-first-flask-app.herokuapp.com//image")
+    return render_template("new_page.html",display_string=display_string,img=img)
 
 @app.get("/hello")
 def hello():
     return "Hello" 
 
-@app.post("/name")
+@app.post("/image")
 def image():
  
     x = [1,2,3,4,5]
@@ -40,15 +41,10 @@ def image():
     plt.ylabel("squares",fontsize=10)
     plt.xticks(fontsize = 10)
     plt.yticks(fontsize = 10)
-    img = io.BytesIO()
-    plt.savefig(img)
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-    return render_template('new_page.html', imagen={ 'imagen': plot_url })
 
-    # img = "static\\plotted_squares.jpg"
-    # plt.savefig(img)
-    # return send_file(img,as_attachment=True,mimetype='image/jpg') 
+    img = "static\\plotted_squares.jpg"
+    plt.savefig(img)
+    return send_file(img,as_attachment=True,mimetype='image/jpg') 
   
 if __name__ == "__main__":
     app.run(debug=True)
