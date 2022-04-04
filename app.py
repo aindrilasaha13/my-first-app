0b820d267ff37dc3f1e8f8975d04a29fa699413f
Aindrila <aindrila.saha.kgec@gmail.com>
2022-04-04 12:39:19 +0530

    testing image passing

--- app.py
+++ app.py
@@ -9,26 +9,26 @@ app = Flask(__name__)
 def add():
     return render_template("home.html")
 
-@app.post("/name")
-def name():
-    text_response = requests.get("https://testing-first-flask-app.herokuapp.com//hello")
-    name = request.form.get("name")
-    display_string = text_response.text + " "+name
+# @app.post("/name")
+# def name():
+#     text_response = requests.get("https://testing-first-flask-app.herokuapp.com//hello")
+#     name = request.form.get("name")
+#     display_string = text_response.text + " "+name
     
-    img_response = requests.get("https://testing-first-flask-app.herokuapp.com//image")
-    # file = open("static\\downloaded.jpg", "wb")
-    # file.write(img_response.content)
-    # file.close()
-    # img = "/static/downloaded.jpg"
+#     img_response = requests.get("https://testing-first-flask-app.herokuapp.com//image")
+#     # file = open("static\\downloaded.jpg", "wb")
+#     # file.write(img_response.content)
+#     # file.close()
+#     # img = "/static/downloaded.jpg"
 
-    plot_url = img_response.content.decode('utf-8')
-    return render_template('new_page.html',display_string=display_string, images={ 'image': plot_url })
+#     plot_url = img_response.content.decode('utf-8')
+#     return render_template('new_page.html',display_string=display_string, images={ 'image': plot_url })
 
 @app.get("/hello")
 def hello():
     return "Hello" 
 
-@app.post("/image")
+@app.post("/name")
 def image():
  
     x = [1,2,3,4,5]
@@ -45,13 +45,11 @@ def image():
     img = io.BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('new_page.html',images={ 'image': plot_url })
     
-    buffer = b''.join(img)
-    b2 = base64.b64encode(buffer)
-    return b2
-    # plot_url = base64.b64encode(img.getvalue()).decode()
-    # return render_template('new_page.html',images={ 'image': plot_url })
-    # return plot_url
+    return plot_url
     
     # img = "static\\plotted_squares.jpg"
     # plt.savefig(img)
